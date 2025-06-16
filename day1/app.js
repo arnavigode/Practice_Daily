4e9ed1301c522b649794d2ab60e0ac119885de5d
arnavigode <arnavi.gode96@gmail.com>
2025-06-16 16:25:12 +0530

    day1

---
+++ day1/app.js
@@ -0,0 +1,6 @@
+
+// react in js
+
+const heading = React.createElement("h1", {id:'heading'}, "Hello World from React");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
