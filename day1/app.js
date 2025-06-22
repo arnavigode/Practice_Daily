d20b5f5f3bf8e4d4e2ed6829fe789401dca24700
arnavigode <arnavi.gode96@gmail.com>
2025-06-22 15:11:33 +0530

    day3

--- day1/app.js
+++ day1/app.js
@@ -1,31 +1,41 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
 // react in js
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React"
-);
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Hello World from React"
+// );
 
-/**
- * 
- * <div id="parent">
-    <div id="child">
-        <h1>h1 tag created</h1>
-        <h2>h2 tag created</h2>
-    </div>
-</div>
- * 
- */
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    [React.createElement("h1", { id: "heading" }, "h1 tag created"), React.createElement("h2", { id: "heading2" }, "h2 tag created"), ]
-  )
-);
+// /**
+//  * 
+//  * <div id="parent">
+//     <div id="child">
+//         <h1>h1 tag created</h1>
+//         <h2>h2 tag created</h2>
+//     </div>
+// </div>
+//  * 
+//  */
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent", key:'a1' },
+//   React.createElement(
+//     "div",
+//     { id: "child", key:'a11' },
+//     [React.createElement("h1", { id: "heading", key:'a10' }, "h1 tag created"), React.createElement("h2", { id: "heading2", key:'a0' }, "h2 tag created"), ]
+//   )
+// );
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
-root.render(parent);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// // root.render(heading);
+// root.render(parent);
+
+
+
+
+// lecr3
+
+
